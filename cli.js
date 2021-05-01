5781d92975dcc4686482452ea037af6dcb771508
chrisrzhou <chrisrzhou@pm.me>
2021-04-30 21:27:44 -0700

    feat: add tsconfig.json file, lint project, use minimist for args parsing

--- cli.js
+++ cli.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 import { validateArgs } from './lib/args.js';
 import { validateConfig } from './lib/config.js';
 import {
@@ -8,19 +9,19 @@ import {
   EDITORCONFIG_FILE,
   GITHUB_WORKFLOW_FILE,
   GITIGNORE_FILE,
-  INDEX_D_TS_FILE,
   INDEX_FILE,
   INDEX_TEST_FILE,
   LICENSE_FILE,
   PACKAGE_JSON_FILE,
   README_FILE,
+  TSCONFIG_JSON_FILE,
 } from './lib/constants.js';
 import { copyFile, createFolder, touchFile, writeFile } from './lib/file.js';
 import { createLicense } from './lib/license.js';
 import { createPackage } from './lib/package.js';
 import { createReadme } from './lib/readme.js';
 
-const init = () => {
+const run = () => {
   const args = validateArgs();
   if (args) {
     const config = validateConfig(args.config);
@@ -34,13 +35,13 @@ const init = () => {
     createFolder(LIB_FOLDER);
     createFolder(TEST_FOLDER);
 
-    touchFile(INDEX_D_TS_FILE);
     touchFile(INDEX_FILE);
     touchFile(INDEX_TEST_FILE);
     copyFile(GITHUB_WORKFLOW_FILE);
     copyFile(EDITORCONFIG_FILE);
     copyFile(GITIGNORE_FILE);
+    copyFile(TSCONFIG_JSON_FILE);
   }
 };
 
-init();
+run();
